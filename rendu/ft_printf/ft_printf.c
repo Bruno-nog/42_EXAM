c3ad1381c2f0be7c60900ebe89bf787df44d965a
Bruno Nogueira de Queiroz <130010090+Bruno-nog@users.noreply.github.com>
2025-04-01 18:50:43 +0000

    d

--- rendu/ft_printf/ft_printf.c
+++ rendu/ft_printf/ft_printf.c
@@ -1,45 +1,51 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 
-void my_printf(const char *format, ...) {
+void    ft_putchar(char c, int *len) {
+    write(1, &c, 1);
+    (*len)++;
+}
+
+void    ft_putstr(char *s, int *len) {
+    if (!s) s = "(null)";
+    while (*s) ft_putchar(*s++, len);
+}
+
+void    ft_putnbr(long n, int *len) {
+    if (n < 0) {
+        ft_putchar('-', len);
+        n = -n;
+    }
+    if (n >= 10)
+        ft_putnbr(n / 10, len);
+    ft_putchar(n % 10 + '0', len);
+}
+
+void    ft_puthex(unsigned int n, int *len) {
+    char *hex = "0123456789abcdef";
+    if (n >= 16)
+        ft_puthex(n / 16, len);
+    ft_putchar(hex[n % 16], len);
+}
+
+int     ft_printf(const char *format, ...) {
+    int len = 0;
     va_list args;
     va_start(args, format);
 
     while (*format) {
         if (*format == '%') {
-            format++; // Avança para o próximo caractere após '%'
-            
-            switch (*format) {
-                case 's': { // String
-                    char *s = va_arg(args, char*);
-                    printf("%s", s);
-                    break;
-                }
-                case 'd': { // Inteiro
-                    int d = va_arg(args, int);
-                    printf("%d", d);
-                    break;
-                }
-                case 'c': { // Caractere
-                    int c = va_arg(args, int); // Promovido para int
-                    printf("%c", c);
-                    break;
-                }
-                case '%': { // Literal '%'
-                    putchar('%');
-                    break;
-                }
-                default: { // Especificador desconhecido
-                    putchar('%');
-                    putchar(*format);
-                    break;
-                }
-            }
+            format++;
+            if (*format == 's') ft_putstr(va_arg(args, char *), &len);
+            else if (*format == 'd') ft_putnbr(va_arg(args, int), &len);
+            else if (*format == 'x') ft_puthex(va_arg(args, unsigned int), &len);
+            else if (*format == 'c') ft_putchar(va_arg(args, int), &len);
+            else if (*format == '%') ft_putchar('%', &len);
+            format++;
         } else {
-            putchar(*format); // Caracteres normais
+            ft_putchar(*format++, &len);
         }
-        format++;
     }
-
     va_end(args);
+    return (len);
 }
