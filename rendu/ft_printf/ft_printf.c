084f397aa6ddd1c5f4e352e6fad3f06a77877bf7
Bruno Nogueira de Queiroz <130010090+Bruno-nog@users.noreply.github.com>
2025-04-01 18:48:37 +0000

    .

--- rendu/ft_printf/ft_printf.c
+++ rendu/ft_printf/ft_printf.c
@@ -1,7 +1,45 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-int main(int ac, char **av)
-{
-    write (1, "nothing\n", 8);
-    return (0);
+void my_printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    while (*format) {
+        if (*format == '%') {
+            format++; // Avança para o próximo caractere após '%'
+            
+            switch (*format) {
+                case 's': { // String
+                    char *s = va_arg(args, char*);
+                    printf("%s", s);
+                    break;
+                }
+                case 'd': { // Inteiro
+                    int d = va_arg(args, int);
+                    printf("%d", d);
+                    break;
+                }
+                case 'c': { // Caractere
+                    int c = va_arg(args, int); // Promovido para int
+                    printf("%c", c);
+                    break;
+                }
+                case '%': { // Literal '%'
+                    putchar('%');
+                    break;
+                }
+                default: { // Especificador desconhecido
+                    putchar('%');
+                    putchar(*format);
+                    break;
+                }
+            }
+        } else {
+            putchar(*format); // Caracteres normais
+        }
+        format++;
+    }
+
+    va_end(args);
 }
